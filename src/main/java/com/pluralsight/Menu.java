60675d15f2f80d6fb2f65827f819b393ff0e9ff9
GanglyChimp <bryan.bigjazzdaddy@gmail.com>
2026-05-27 07:40:51 -0800

    added menu items

---
+++ src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Menu
+{
+    public static final String[] Bread =
+        {
+         "white", "wheat","rye", "wrap"
+        };
+
+    public static final String[] Meat=
+       {
+         "Steak", "Ham", "Salami", "Roast Beef", "Chicken", "Bacon"
+       };
+
+    public static final String[] Cheese=
+       {
+        "American", "Provolone", "Cheddar", "Swiss", "PepperJack"
+       };
+    public static final String[] Topping=
+       {
+        "Lettuce", "Peppers", "Onions", "Tomatoes", "Jalapenos", "Cucumbers", "Pickles",
+           "Guacamole", "Mushrooms"
+       };
+
+    public static final String[] Sauces=
+       {
+         "Mayo", "Mustard", "Ketchup", "Ranch", "Thousand Islands", "Vinaigrette"
+       };
+    public static final String[] SizesInch=
+       {
+          "4", "8", "12"
+       };
+    public static final String[] Drink=
+       {
+        "water", "coke", "diet coke", "sprite", "orange juice", "lemonade", "iced tea"
+       };
+    public static final String[] DrinkSize=
+      {
+        "Small", "Medium", "Large"
+      };
+}
