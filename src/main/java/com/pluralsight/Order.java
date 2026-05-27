15d4415d944b093615a6bc880e267b728ff8b539
GanglyChimp <bryan.bigjazzdaddy@gmail.com>
2026-05-27 09:44:26 -0800

    initial commit, constructors and stream methods added. included list reversal for easy display and removal of any new items

---
+++ src/main/java/com/pluralsight/Order.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import com.pluralsight.Terminal.MenuItem;
+
+public class Order
+{
+    private List<MenuItem> items;
+    public Order()
+    {
+        this.items=new ArrayList<>();
+    }
+
+    //add to the users order
+    public void addItem(MenuItem item)
+    {
+        items.add(item);
+    }
+
+    //returning the items
+    public List<MenuItem>getItems()
+    {
+        return items;
+    }
+
+    //we want to see what the latest entry was, makes it easier to remove recent decisions
+    public List<MenuItem>getItemsNewestFirst()
+    {
+        List<MenuItem>reversed = new ArrayList<>(items);
+        Collections.reverse(reversed);
+        return reversed;
+        //we reverse the order of the list to see the newest first
+    }
+
+    //getting the total price of all items
+    public double getTotal()
+    {
+        return items.stream()
+                .mapToDouble(MenuItem::getPrice)
+                .sum();
+        //adds the sum of the items
+    }
+
+    //clear all items
+    public void cancelOrder()
+    {
+        items.clear();
+        //simple .clear wipes the items list
+
+    }
+}
