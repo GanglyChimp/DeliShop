dabc22f973102441a312223ab75c9cefa20f66dd
GanglyChimp <bryan.bigjazzdaddy@gmail.com>
2026-05-27 08:44:39 -0800

    added chips, cleaned up naming

--- src/main/java/com/pluralsight/Menu.java
+++ src/main/java/com/pluralsight/Menu.java
@@ -2,27 +2,27 @@ package com.pluralsight;
 
 public class Menu
 {
-    public static final String[] Bread =
+    public static final String[] breadType =
         {
          "white", "wheat","rye", "wrap"
         };
 
-    public static final String[] Meat=
+    public static final String[] meatType=
        {
          "Steak", "Ham", "Salami", "Roast Beef", "Chicken", "Bacon"
        };
 
-    public static final String[] Cheese=
+    public static final String[] cheeseType=
        {
         "American", "Provolone", "Cheddar", "Swiss", "PepperJack"
        };
-    public static final String[] Topping=
+    public static final String[] toppings=
        {
         "Lettuce", "Peppers", "Onions", "Tomatoes", "Jalapenos", "Cucumbers", "Pickles",
            "Guacamole", "Mushrooms"
        };
 
-    public static final String[] Sauces=
+    public static final String[] sauceType=
        {
          "Mayo", "Mustard", "Ketchup", "Ranch", "Thousand Islands", "Vinaigrette"
        };
@@ -30,12 +30,16 @@ public class Menu
        {
           "4", "8", "12"
        };
-    public static final String[] Drink=
+    public static final String[] drinkType=
        {
         "water", "coke", "diet coke", "sprite", "orange juice", "lemonade", "iced tea"
        };
-    public static final String[] DrinkSize=
+    public static final String[] drinkSize=
       {
         "Small", "Medium", "Large"
+      };
+    public static final String[] chipsType=
+      {
+       "BBQ", "Plain", "Sour Cream & Onion","Salt & Vinegar","Jalapeno","Cheddar","Cool Ranch","Honey Mustard"
       };
 }
