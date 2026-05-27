d7e8ffd08520066221bc642b80de032f0cf85070
GanglyChimp <bryan.bigjazzdaddy@gmail.com>
2026-05-27 14:17:20 -0800

    test for sizes

--- src/test/java/com/pluralsight/OrderScreenTest.java
+++ src/test/java/com/pluralsight/OrderScreenTest.java
@@ -14,6 +14,36 @@ class OrderScreenTest
         int breadChoice = 1;
         String bread = Menu.breadType[breadChoice - 1];
         assertEquals("white", bread);
+
+
+    }
+
+    @Test
+    public void testSizeSelectionFourInch() {
+        int sizeChoice = 1;
+        String size = Menu.SizesInch[sizeChoice - 1];
+        assertEquals("4", size);
+    }
+
+    @Test
+    public void testSizeSelectionEightInch() {
+        int sizeChoice = 2;
+        String size = Menu.SizesInch[sizeChoice - 1];
+        assertEquals("8", size);
+    }
+
+    @Test
+    public void testSizeSelectionTwelveInch() {
+        int sizeChoice = 3;
+        String size = Menu.SizesInch[sizeChoice - 1];
+        assertEquals("12", size);
+    }
+
+    @Test
+    public void testSizeSelectionBoundary() {
+        int sizeChoice = Menu.SizesInch.length;
+        String size = Menu.SizesInch[sizeChoice - 1];
+        assertEquals("12", size);
     }
 
 
